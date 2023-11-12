125a1b25442984221fcdcb7858a8f3a8420b400a
Emerson <emersoncoelho95@gmail.com>
2023-11-12 07:34:01 -0300

    Adicionando loading

--- script.js
+++ script.js
@@ -7,9 +7,12 @@ async function buscarPoke() {
     // Limpar a lista de números gerados
     numerosGerados = [];
 
+    // Adicionar indicador de "loading"
+    document.querySelector("#botaoGerar").innerHTML = "Gerando...";
+
     for (let i = 0; i < 6; i++) {
         let numeroAleatorio;
-        
+
         // Garantir que o número gerado não se repita
         do {
             numeroAleatorio = Math.floor(Math.random() * 150) + 1;
@@ -21,9 +24,10 @@ async function buscarPoke() {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${numeroAleatorio}`);
         const data = await response.json();
         mostraPoke(data);
-
-        document.querySelector("#botaoGerar").innerHTML = "Gerar novamente";
     }
+
+    // Remover indicador de "loading" e alterar o texto do botão
+    document.querySelector("#botaoGerar").innerHTML = "Gerar novamente";
 }
 
 // Restante do código permanece o mesmo
